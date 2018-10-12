d92382a2550d0bb6d982ecdf0140e1b97def3f5c
Arun Seehra <seehra.a@gmail.com>
2018-10-12 11:19:49 -0500

    Make error test cases clearer

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,14 +6,11 @@ const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function () {
   it('should throw if called with a string', function () {
-    expect(() => {
-      fizzBuzzer('Im not a number!');
-    }).to.throw(Error);
+    // expect(fizzBuzzer('a string')).to.throw(Error);
+    expect(() =>  fizzBuzzer('Im not a number!')).to.throw(Error);
   });
 
   it('should throw if called with an array', function () {
-    expect(() => {
-      fizzBuzzer([]);
-    }).to.throw(Error);
+    expect(() => fizzBuzzer([])).to.throw(Error);
   });
 });
