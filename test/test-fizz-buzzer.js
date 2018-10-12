6e9deaba56024b574b3a2086a18048f20ea4bc8c
Arun Seehra <seehra.a@gmail.com>
2018-10-12 11:20:41 -0500

    Test fizz-buzz cases

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -13,4 +13,16 @@ describe('fizzBuzzer', function () {
   it('should throw if called with an array', function () {
     expect(() => fizzBuzzer([])).to.throw(Error);
   });
+
+  it('should return "fizz-buzz" for only multiples of 15', function () {
+    const goodInputs = [15, 30, 45, 120];
+    goodInputs.forEach(function (input) {
+      expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+    });
+
+    const badInputs = [3, 9, 20];
+    badInputs.forEach(function (input) {
+      expect(fizzBuzzer(input)).to.not.equal('fizz-buzz');
+    });
+  });
 });
